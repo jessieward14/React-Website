80793aae1717c569eb300b501598ea11945aa552
jessieward <jessica_ward25@milton.edu>
2024-12-10 08:18:17 -0500

    homework#

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,12 @@
+import {useState} from "react"
+import {Button} from "../Components/Button"
+import {StoreItem} from "../Components/StoreItem"
+
+
+export function Homepage(){
+    return(
+        <>
+        <h1> This is the home page!</h1>
+        </>
+    )
+}
---
+++ src/Pages/Page2.js
@@ -0,0 +1,7 @@
+export function Page2(){
+    return(
+        <>
+        <h1> This is the second page!</h1>
+        </>
+    )
+}
