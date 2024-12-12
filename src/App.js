5e1fb6bdc85fdd8c0bb3ea4552b96b141afa34ab
jessieward <jessica_ward25@milton.edu>
2024-12-12 11:27:25 -0500

    homework

--- src/App.js
+++ src/App.js
@@ -1,38 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import React from 'react';
-import './App.css'
+// import logo from './logo.svg';
+// import './App.css';
+// import axios from 'axios';
+// import React from 'react';
+// import Navbar from "./Navbar"
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+// import Home from "./Pages";
+// import About from "./Pages/About ";
+// import Blog from "./Pages/blog";
+// import Signup from "./Pages/signup";
+// import Contact from "./Pages/contact";
+import React from "react";
+import Navbar from "./Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./Pages";
+import About from "./Pages/About";
+import Blog from "./Pages/blog";
+import SignUp from "./Pages/signup";
+import Contact from "./Pages/contact";
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
+
+
+
+// import './App.css'
+
+
+
+
+// function App() {
+  
+//   return (
+//     <Router>
+//             <Navbar />
+//             <Routes>
+//                 <Route exact path="/" element={<Home />} />
+//                 <Route path="/About" element={<About />} />
+//                 <Route
+//                     path="/contact"
+//                     element={<Contact />}
+//                 />
+//                 <Route path="/blog" element={<Blog />} />
+//                 <Route
+//                     path="/signup"
+//                     element={<Signup />}
+//                 />
+//             </Routes>
+//         </Router>
+   
+//   );
+// }
+
+// export default App;
+
 
 
 function App() {
-  
-  return (
-    <div className="App">
-    <h1> Welcome to my app</h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <MyButton />
-    </div>
-  );
+    return (
+        
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/About" element={<About />} />
+                <Route
+                    path="/contact"
+                    element={<Contact />}
+                />
+                <Route path="/blog" element={<Blog />} />
+                <Route
+                    path="/signup"
+                    element={<SignUp />}
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Pages/blog.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+const Blog = () => {
+    return <h1>You can write your blogs!</h1>;
+};
+
+export default Blog;
