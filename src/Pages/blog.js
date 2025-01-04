302e81c6aaaf51e0245055fa297df3cbb1e5ddf7
jessieward <jessica_ward25@milton.edu>
2025-01-04 13:31:04 -0500

    homework

--- src/Pages/blog.js
+++ src/Pages/blog.js
@@ -3,37 +3,47 @@ import axios from 'axios';
 import './blog.css'
 
 
+
 const Blog=()=>{
     const [Data,setData]=useState({
         
         Image:''
     })
     
-
-    useEffect(()=>{
+        const changeImage = () => {
     axios.get('https://foodish-api.com/api/')
         .then(res=>{
-            console.log('Response from main API: ',res)
-            console.log('Image Data: ',res.data.image)
-            let imageData=res.data.image;
-            setData({Image:imageData})
-        })
-        .catch(err=>{
+            console.log('Response from API: ', res);
+            console.log('Image Data: ', res.data.image);
+            let imageData = res.data.image;
+            setData({ Image: imageData });
+          })
+          .catch(err => {
             console.log(err);
-        })
-        
-},[])
+          });
+};
+
+useEffect(() => {
+    changeImage(); // Fetch an image when the component mounts
+  }, []);
 
 
 
 return (
     <div className="blog">
        {/* <h1>{Data.Image}</h1> */}
-       <img src={Data.Image} alt="food" height = {400} width='auto'/>
-       
-
+       <h1> Here is some food!</h1>
+       <img
+        src={Data.Image}
+        alt="food"
+        height={400}
+        width='auto'
+        onClick={changeImage} // Fetch a new image when clicked
+      />
         {/* //console.log(image.res); */}
-       <p> hi</p>
+        
+        <p>Click the image to load a new one!</p>
+
     
     </div>
 );
--- src/Pages/signup.js
+++ src/Pages/signup.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './signUp.css'; // Y
 
+// instead of one const of image in Blog there are 2:setup and punchline as that is how the api works
 const SignUp = () => {
-    return (
-        <div>
-            <h1>Sign Up Successful</h1>
-        </div>
-    );
+    const [joke, setJoke] = useState({ //instead of data in blog - changed to joke for this
+      setup: '',
+      punchline: '',
+    });
+    const [showPunchline, setShowPunchline] = useState(false);//this shows the answer
+  
+// followed structure of blog
+    const fetchJoke = () => {
+      axios.get('https://official-joke-api.appspot.com/random_joke')
+        .then(res => {
+          console.log(res.data);
+          const { setup, punchline } = res.data;
+          setJoke({ setup, punchline});
+          setShowPunchline(false); // Hide punchline to start
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+  
+
+const handleClickSetup = () => {
+    setShowPunchline(true);
+  };//this makes it so that when I click, I get the answer/punchline
+
+  const handleNewJokeClick = () => {
+    fetchJoke();
+  };//gets new joke
+
+  useEffect(() => {
+    fetchJoke();
+  }, []);
+
+
+
+return (
+    <div className="sign-up">
+      <div className="joke-container">
+        <h2>Oh! You want a joke to go with your meal?? I've got just the ones for you!</h2>
+        <p className="joke-setup" onClick={handleClickSetup}>
+          {joke.setup}
+        </p>
+        {showPunchline && <p className="joke-punchline">{joke.punchline}</p>}
+      </div>
+      <button className="new-joke-button" onClick={handleNewJokeClick}>
+        New Joke
+      </button>
+    </div>
+  );
 };
+
 
 export default SignUp;
