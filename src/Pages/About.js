b1be29eb6f754faa8d5fed8768aa12600188d9e1
jessieward <jessica_ward25@milton.edu>
2025-01-21 08:55:18 -0500

    Final Project

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,8 +2,20 @@ import React from "react";
 
 const About = () => {
     // what I am printing in the about me page
+   
+    document.body.style.backgroundColor = "rgb(185,205,230)";
+    document.body.style.marginBottom = "0px";
+    document.body.style.textAlign = "center";
+    document.body.style.padding = "20 px";
+   
+    
+
+
+
+    
     return (
-        <div>
+        <div className = "About">
+
             <h1>
                 What to know about my website?
             </h1>
@@ -11,6 +23,9 @@ const About = () => {
             <p> See the Jokes page if you want some good jokes!</p>
         </div>
     );
+
+    
+    
 };
 
 export default About;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -4,9 +4,12 @@ import React from "react";
 
 //what shows up when you npm run start
 const Home = () => {
+    document.body.style.textAlign = "center";
+    document.body.style.fontSize = "23px";
+
     return (
         <div>
-            <h1>Welcome to Jessie's Website!</h1>
+            <h1 >Welcome to Jessie's Website!</h1>
         </div>
     );
 };
